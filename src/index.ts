a18ac44a7c1afbe33d704dad1344e3d872bd435a
Veronica Rojas <ar_veronica@yahoo.com>
2023-10-20 11:55:38 +0100

    add list item function

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
 import { v4 as uuidv4 } from "uuid"
 
+
+type Task = {
+  id: string,
+  title: string, 
+  completed: boolean, 
+  createdAt: Date
+}
+
 const list = document.querySelector<HTMLUListElement>("#list")
 const form = document.querySelector<HTMLFormElement>("new-task-form")
 const input = document.querySelector<HTMLInputElement>("new-task-title")
@@ -9,11 +17,22 @@ form?.addEventListener("submit", e => {
 
   if(input?.value == "" || input?.value == null) return
 
-  const task = {
+  const newTask: Task = {
     id: uuidv4(),
     title: input.value,
     completed: false,
     createdAt: new Date()
   }
  
+  addListItem(newTask)
 })
+
+function addListItem(task : Task){
+const item = document.createElement("li")
+const label = document.createElement('label')
+const checkbox = document.createElement('input')
+checkbox.type = "checkbox"
+label.append(checkbox, task.title)
+item.append(label)
+list?.append(item)
+}
