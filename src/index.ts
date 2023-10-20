97a8931f08a0ee773f2b4934f70b23641286a017
Veronica Rojas <ar_veronica@yahoo.com>
2023-10-20 11:39:42 +0100

    select html elements and creare task object assigning types' '

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { v4 as uuidv4 } from "uuid"
 
-import confetti from 'canvas-confetti';
+const list = document.querySelector<HTMLUListElement>("#list")
+const form = document.querySelector<HTMLFormElement>("new-task-form")
+const input = document.querySelector<HTMLInputElement>("new-task-title")
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+form?.addEventListener("submit", e => {
+  e.preventDefault()
+
+  if(input?.value == "" || input?.value == null) return
+
+  const task = {
+    id: uuidv4(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date()
+  }
+ 
+})
